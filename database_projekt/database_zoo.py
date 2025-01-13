8aab8fd59306ccdf255136267cdf83e24a2a8f7c
2sn00py4u <felix.werner.t@gmx.de>
2025-01-13 12:19:39 +0100

    initial_commit

---
+++ database_projekt/database_zoo.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+class zoo:
+    def __init__(self, data_files:list = []):
+        pass
+                
+
+def main():
+    Zoo = zoo(["database_projekt\\Zoo_test.xlsx"])
+
+if __name__ == "__main__":
+    main()
