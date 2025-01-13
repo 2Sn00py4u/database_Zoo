41767e61b080ccdd7099568b83571e288cc965ee
2sn00py4u <felix.werner.t@gmx.de>
2025-01-13 12:46:16 +0100

    real animals csv + structure

--- database_projekt/database_zoo.py
+++ database_projekt/database_zoo.py
@@ -1,12 +1,23 @@
-import sqlite3
+import sqlite3, os
+
+ANIMAL_PATH = "database_projekt\\animals.csv"
 
 class zoo:
-    def __init__(self, data_files:list = []):
-        pass
+    def __init__(self, animals:str):
+        def init_animals(animals:str):
+            animal_table = []
+            with open(animals, "r") as animal_data:
+                for line in list(animal_data)[1:]:
+                    line = line.split(";")
+                    animal_table.append(line)
+            print(animal_table)
+        init_animals(animals)
+                    
+        
                 
 
 def main():
-    Zoo = zoo(["database_projekt\\Zoo_test.xlsx"])
+    Zoo = zoo(ANIMAL_PATH)
 
 if __name__ == "__main__":
     main()
